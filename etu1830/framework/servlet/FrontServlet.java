94bac6a09e5dfeaf3d19b8c03c71c5f544702ef7
ETU-1830-Renato-Parc-informatique <119660851+Renato2508@users.noreply.github.com>
2023-03-14 07:37:53 +0100

    sprint1: web.xml et frontServlet.java

---
+++ etu1830/framework/servlet/FrontServlet.java
@@ -0,0 +1,35 @@
+package etu1830.framework.servlet;
+
+import etu1830.framework.Mapping;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FrontServlet extends HttpServlet {
+    
+    protected void processRequest(HttpServletRequest req,
+    HttpServletResponse res) throws IOException, ServletException{
+        PrintWriter out = res.getWriter();
+        out.print("Bienvenue<br>");
+        
+    }
+
+    public void doGet(HttpServletRequest req,
+                      HttpServletResponse res)
+        throws IOException, ServletException{
+        processRequest(req, res);
+    }
+
+    public void doPost(HttpServletRequest req,
+                      HttpServletResponse res)
+        throws IOException, ServletException{
+        processRequest(req, res);
+    }
+}
+
