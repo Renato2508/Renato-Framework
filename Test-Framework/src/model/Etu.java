801f9bbdd4122401b15f81201f293d6ddde8f941
ETU-1830-Renato-Parc-informatique <119660851+Renato2508@users.noreply.github.com>
2023-03-21 06:21:30 +0100

    sprint4: restructuration du repertoire en framework et test-framework

---
+++ Test-Framework/src/model/Etu.java
@@ -0,0 +1,15 @@
+package model;
+
+import etu1830.annotation.Urls;
+
+public class Etu {
+    @Urls(url = "inscrire/etudiant")
+    public void Inscrire(){
+
+    }   
+    
+    public void desinscrire(){
+        
+    }
+
+}
---
+++ Test-Framework/src/model2/Emp.java
@@ -0,0 +1,10 @@
+package model2;
+
+import etu1830.annotation.Urls;
+
+public class Emp {
+    @Urls(url = "emp-myMethod")
+    public void maMethode(){
+
+    }
+}
