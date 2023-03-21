3c59e7667085af59ac7ce15cf9d8960765c765c2
Renato2508 <119660851+Renato2508@users.noreply.github.com>
2023-03-21 07:41:31 +0100

    Delete etu1830 directory
    Adaptation a la structure normalisee

--- etu1830/annotation/Urls.java
+++
@@ -1,9 +0,0 @@
-package etu1830.annotation;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME) // durée de vie ( execution )
-@Target(ElementType.METHOD)  //ne peut être utilisée que sur des methodes de classe.
-public @interface Urls{
-    String url() default "";
-}
