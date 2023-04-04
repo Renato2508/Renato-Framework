e7d83bd928b557f09d1a60b7cfcec78328f36d0e
ETU-1830-Renato-Parc-informatique <119660851+Renato2508@users.noreply.github.com>
2023-04-04 08:29:36 +0200

    SPRINT5: correctif pour l'instanciation du ModelView

--- Test-Framework/src/Empe.java
+++ Test-Framework/src/Empe.java
@@ -1,10 +1,13 @@
 package model2;
 
 import etu1830.annotation.Urls;
+import etu1830.framework.ModelView;
 
 public class Empe {
     @Urls(url = "emp-myMethod")
-    public String maMethode(){
-        return "emp-list.jsp";
+    public ModelView maMethode(){
+        ModelView mv = new ModelView();
+        mv.setView("emp-list.jsp");
+        return mv;
     }
 }
--- Test-Framework/src/Etu.java
+++ Test-Framework/src/Etu.java
@@ -1,11 +1,14 @@
 package model;
 
 import etu1830.annotation.Urls;
+import etu1830.framework.ModelView;
 
 public class Etu {
     @Urls(url = "inscrire-etudiant")
-    public String Inscrire(){
-        return "emp-list.jsp";    
+    public ModelView Inscrire(){
+        ModelView mv = new ModelView();
+        mv.setView("emp-list.jsp");
+        return mv;
     }   
     
     public void desinscrire(){
