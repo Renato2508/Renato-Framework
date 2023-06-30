61eaa9788b4798076d374b16d082de8df6c224b4
ETU-1830-Renato-Parc-informatique <119660851+Renato2508@users.noreply.github.com>
2023-06-30 18:04:48 +0200

    SPRINT 11: reglementation des acces aux methodes d'action

---
+++ Framework/src/Auth.java
@@ -0,0 +1,10 @@
+package etu1830.annotation;
+
+import java.lang.annotation.*;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Auth {
+    String profile() default "";
+}
--- Framework/src/ModelView.java
+++ Framework/src/ModelView.java
@@ -5,15 +5,23 @@ import java.util.HashMap;
 public class ModelView{
     String view;
     HashMap<String, Object> data = new HashMap<String, Object>();
+    HashMap<String, Object > session = new HashMap<String, Object>();
 
     public ModelView() {
     }
     
 
     // _ _ _ Methods _ _ _
+
+    public void addSessionItem(String key, Object value){
+        this.session.put(key, value);
+    }
+
     public void addItem(String key, Object value){
         this.data.put(key, value);
     }
+
+
     // _ _ _ get set _ _ _ 
     
 
@@ -33,6 +41,16 @@ public class ModelView{
 
     public void setData(HashMap<String, Object> data) {
         this.data = data;
+    }
+
+
+    public HashMap<String, Object> getSession() {
+        return session;
+    }
+
+
+    public void setSession(HashMap<String, Object> session) {
+        this.session = session;
     }
     
 }
